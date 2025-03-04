59e7e39f4d658b0901b1aacee074e49a7f7392c9
Chris Cutts <github@chriscutts.uk>
2025-03-04 16:16:51 +0000

    Fix filament and flowbite

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -9,30 +9,29 @@
 {{--    </ul>--}}
 {{--</div>--}}
 <nav>
-        <button data-collapse-toggle="navbar-hamburger" type="button" class="inline-flex cursor-pointer items-center justify-center p-2 w-10 h-10 text-sm  rounded-lg" aria-controls="navbar-hamburger" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5 text-dark-green" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-        </button>
-        <div class="hidden w-full absolute z-50 bg-medium-green" id="navbar-hamburger">
-            <ul class="flex flex-col font-medium mt-4 rounded-lg" id="mobile-nav">
-                <li>
-                    <a href="/" class="block py-2 px-3 {{ Request::is('/') ? 'bg-light-green' : '' }}" aria-current="page">Home</a>
-                </li>
-                <li>
-                    <a href="/about-us" class="block py-2 px-3 {{ Request::is('about-us') ? 'bg-light-green' : '' }}">About Us</a>
-                </li>
-                <li>
-                    <a href="/venue" class="block py-2 px-3 {{ Request::is('venue') ? 'bg-light-green' : '' }}">Venue</a>
-                </li>
-                <li>
-                    <a href="/order-of-service" class="block py-2 px-3 {{ Request::is('order-of-service') ? 'bg-light-green' : '' }}">Order of Service</a>
-                </li>
-                <li>
-                    <a href="{{ route('rsvp', ['rsvp' => '019559f6-a374-72c5-ae21-03abb7be7cb4']) }}" class="block py-2 px-3 {{ Request::is('rsvp') ? 'bg-light-green' : '' }}">RSVP</a>
-                </li>
-            </ul>
-        </div>
+    <button data-collapse-toggle="navbar-hamburger" type="button" class="inline-flex cursor-pointer items-center justify-center p-2 w-10 h-10 text-sm  rounded-lg" aria-controls="navbar-hamburger" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5 text-dark-green" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full absolute z-50 bg-medium-green" id="navbar-hamburger">
+        <ul class="flex flex-col font-medium mt-4 rounded-lg" id="mobile-nav">
+            <li>
+                <a href="/" wire:navigate class="block py-2 px-3 {{ Request::is('/') ? 'bg-light-green' : '' }}" aria-current="page">Home</a>
+            </li>
+            <li>
+                <a href="/about-us" wire:navigate class="block py-2 px-3 {{ Request::is('about-us') ? 'bg-light-green' : '' }}">About Us</a>
+            </li>
+            <li>
+                <a href="/venue" wire:navigate class="block py-2 px-3 {{ Request::is('venue') ? 'bg-light-green' : '' }}">Venue</a>
+            </li>
+            <li>
+                <a href="/order-of-service" wire:navigate class="block py-2 px-3 {{ Request::is('order-of-service') ? 'bg-light-green' : '' }}">Order of Service</a>
+            </li>
+            <li>
+                <a href="{{ route('rsvp', ['rsvp' => '019559f6-a374-72c5-ae21-03abb7be7cb4']) }}" wire:navigate class="block py-2 px-3 {{ Request::is('rsvp') ? 'bg-light-green' : '' }}">RSVP</a>
+            </li>
+        </ul>
+    </div>
 </nav>
-
