bcf8bb79bdd1fa4805bee5e65a9a4aa836e11fdf
Chris Cutts <github@chriscutts.uk>
2025-03-06 17:43:29 +0000

    Set max width on page

--- resources/views/livewire/page.blade.php
+++ resources/views/livewire/page.blade.php
@@ -1,6 +1,6 @@
 <x-slot:description>{{ $page->seo->description }}</x-slot>
 <x-section>
-    <article class="prose">
+    <article class="prose max-w-full">
         {!! $page->html !!}
     </article>
 </x-section>
