4f6e4ae0f0f74d27d264b6716190b3b30005a678
Chris Cutts <github@chriscutts.uk>
2025-03-24 02:09:37 +0000

    Add title to iframe

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -3,6 +3,7 @@
         <iframe
             style="height: 100%; width: 100%; border: 0"
             frameborder="0"
+            title="Google Maps"
             src="https://www.google.com/maps/embed/v1/place?q=the+croft+hotel+darlington&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
         ></iframe>
     </div>
