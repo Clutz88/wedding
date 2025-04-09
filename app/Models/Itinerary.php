fda5dc510ebc395a8acddb0dec735ad889d5375b
Chris Cutts <github@chriscutts.uk>
2025-04-09 01:23:56 +0100

    Remove seconds from order of service

--- app/Models/Itinerary.php
+++ app/Models/Itinerary.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Itinerary extends Model
 {
     use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'time' => 'datetime',
+        ];
+    }
+
+    protected function time(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->asDateTime($value)->format('H:i')
+        );
+    }
 }
